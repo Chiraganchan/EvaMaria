1d957e2fe8966aa0e0e8636045df6db5f735a6b9
Damodar46 <94790981+Damodar46@users.noreply.github.com>
2021-11-27 07:18:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -448,14 +448,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('ℹ️ ԋҽʅρ', callback_data='help'),
             InlineKeyboardButton('😊 αυႦσυ✞', callback_data='about')
-    [[      
-   
-reply_markup = InlineKeyboardMarkup(buttons)
+            [[
+            reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
+        
     elif query.data == "help":
         buttons = [[
             InlineKeyboardButton('ＭＡＮＵＡＬ ＦＩＬＴＥＲ', callback_data='manuelfilter'),
