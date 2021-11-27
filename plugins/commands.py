552b354ae5595121ccee503b5c84d4c693f3e09c
TechnoKaMagic <91592694+TechnoKaMagic@users.noreply.github.com>
2021-11-27 16:41:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,13 +37,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ αԃԃ ɱҽ ✞σ ყσυɾ ɠɾσυρ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+            InlineKeyboardButton('🔍 ʂҽαɾ८ԋ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🤖 υρԃα✞ҽʂ', url='https://t.me/KR_MOVIES_K')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ ԋҽʅρ', callback_data='help'),
+            InlineKeyboardButton('😊 αυႦσυ✞', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -78,13 +78,13 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ αԃԃ ɱҽ ✞σ ყσυɾ ɠɾσυρ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+            InlineKeyboardButton('🔍 ʂҽαɾ८ԋ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🤖 υρԃα✞ҽʂ', url='https://t.me/KR_MOVIES_K')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ ԋҽʅρ', callback_data='help'),
+            InlineKeyboardButton('😊 αυႦσυ✞', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
