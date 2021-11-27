27c9a232cfc84668bfdec21b11980bfb62c3c247
Damodar46 <94790981+Damodar46@users.noreply.github.com>
2021-11-27 07:14:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -448,8 +448,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('ℹ️ ԋҽʅρ', callback_data='help'),
             InlineKeyboardButton('😊 αυႦσυ✞', callback_data='about')
-            InlineKeyboardButton('🏴cнαηηεℓ', url='https://t.me/KR_MOVIES_K')         
-   [[
+    [[      
+   
 reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
