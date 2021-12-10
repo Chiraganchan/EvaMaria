bf269609bbe6d42405f4c16c28d0acd6fd76810e
Damodar46 <94790981+Damodar46@users.noreply.github.com>
2021-12-10 16:18:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -444,7 +444,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ αԃԃ ɱҽ ✞σ ყσυɾ ɠɾσυρ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 ʂҽαɾ८ԋ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 υρԃα✞ҽʂ', url='https://t.me/KR_MOVIES_K')
+            InlineKeyboardButton('🤖 υρԃα✞ҽʂ', url='https://t.me/KDMMOVIES_KANNADA')
             ],[
             InlineKeyboardButton('🔮gяσυρ', url='https://t.me/Kannada_cinema_clubss')
             ],[
