fdcedde774f12a5e94188696a50bf3f8f0c778dd
Damodar46 <94790981+Damodar46@users.noreply.github.com>
2021-12-24 19:07:04 +0530

    Update afk.py

--- plugins/TechnoKaMagic/afk.py
+++ plugins/TechnoKaMagic/afk.py
@@ -1,10 +1,10 @@
 import random, html
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import (DisableAbleCommandHandler,
+from TechnoKaMagic import dispatcher
+from TechnoKaMagic.modules.disable import (DisableAbleCommandHandler,
                                           DisableAbleMessageHandler)
-from SaitamaRobot.modules.sql import afk_sql as sql
-from SaitamaRobot.modules.users import get_user_id
+from TechnoKaMagic.modules.sql import afk_sql as sql
+from TechnoKaMagic.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
