55d4096121f7598e129517f72575d0938b3a0c6d
Damodar46 <94790981+Damodar46@users.noreply.github.com>
2022-01-10 21:47:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -483,10 +483,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton("ᴘᴜʀɢᴇ", callback_data='Purge'),
             InlineKeyboardButton("ᴡʜᴏɪs", callback_data='id'),
-            InlineKeyboardButton("ᴀʟɪᴠᴇ", callback_data='Alive'),
-            InlineKeyboardButton("Covid", callback_data='covid')
+            InlineKeyboardButton("ᴀʟɪᴠᴇ", callback_data='Alive')
             ],[
             InlineKeyboardButton("ʙᴀɴs", callback_data='Bans'),
+            InlineKeyboardButton("ᴄᴏʀᴏɴᴀ", callback_data='covid')
             InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data='close_data')
             ],[
             InlineKeyboardButton("ʜᴏᴍᴇ", callback_data='start')
